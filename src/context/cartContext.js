a461490b3a50b1a7ce6771b9f869d88d52d79536
Jesus Hector Zamora <jhector.zamora@hotmail.com>
2021-10-29 17:34:38 -0700

    prices and total functionality

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -12,11 +12,33 @@ export function useUpdateCart(){
 }
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([{name: "Gloves", qty: 2}]);
+  const [cart, setCart] = useState([{name: "Gloves", qty: 1, price: 10},{name: "Shoes", qty: 1, price: 20}]);
+
+  const updateCartPlus = (name)=>{
+    setCart(cart.map(item => {
+      if(item.name === name){
+        item.qty++;
+      }
+      return item;
+    }));
+
+  }
+  const updateCartMinus = (name)=>{
+        
+      setCart(cart.map(item => {
+        if(item.name === name){
+          if(item.qty >= 1){
+          item.qty--;
+        }
+      }
+        return item;
+      }));
+      
+  }
 
   return (
     <cartContext.Provider value={cart}>
-      <updateCartContext.Provider value={setCart}>
+      <updateCartContext.Provider value={[updateCartPlus,updateCartMinus]}>
         {children}
       </updateCartContext.Provider>
     </cartContext.Provider>
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,22 +1,50 @@
-import React from "react";
-import { useCart } from "../context/cartContext";
+import React,{useState,useEffect} from "react";
+import { useCart,useUpdateCart } from "../context/cartContext";
 import "../styles/cartStyle.css"
 
 const Cart = () => {
+
+  const [plus,minus] = useUpdateCart();
+  const [total,setTotal] = useState(0);
+
+
+ const cart = useCart();
+
+ useEffect(() => {
+  setTotal(() => {
+    let total = 0;
+    cart.map(item => {
+      total += item.price * item.qty;
+    });
+    return total;
+  })
+ },[cart]);
+
   return (
     <div className="cart-container d-flex flex-column justify-content-center">
       <h1>Cart</h1>
 
       {useCart().map((item) => {
         return (
-          <>
-            <div className="d-flex justify-content-around align-items-center">
+            <div className="d-flex justify-content-center align-items-center product">
               <p>{item.name}</p>
               <p>{item.qty}</p>
+
+              <div className="buttons">
+                <button onClick={()=>plus(item.name)}>+</button>
+                <button onClick={()=>minus(item.name)}>-</button>
+              </div>
+
+              <p> Price: ${item.price} </p>
+
+              <p> <strong> Total: </strong> ${item.qty * item.price}</p>
+
             </div>
-          </>
         );
       })}
+
+      <h1>Your total is: ${total}</h1>
+
     </div>
   );
 };
